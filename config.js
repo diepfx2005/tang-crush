730b233bf5a259b00250b42630add68c9d9219b0
diepfx2005 <85275897+diepfx2005@users.noreply.github.com>
2021-06-03 14:30:15 +0700

    config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    desc: 'Em đồng ý làm ny anh nha 😘 ',
+    btnYes: 'Dạ, em đồng ý 🥰 <33',
+    btnNo: 'Không, Em xl😭 :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Em không cần phải lo chuyện gì nữa, dã có anh đây rồi..',
+    btnAccept: 'Dạ, yêu anh❤️ <3',
+    messLink: 'https://m.me/diepfx2005'
 }
